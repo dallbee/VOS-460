79ed35470e2e5a36f7b1924bb016e460d758428d
Taylor <xTaylorSanchez@gmail.com>
2013-04-09 19:00:28 -0700

    starting assembler class

--- src/assembler/assembler.cpp
+++ src/assembler/assembler.cpp
@@ -1,1 +1,61 @@
+/*
+class assembler{
+public:
 
+private:
+	int  loadi	//0000 OPcode mask
+	int loadi	//0000 OPcode mask
+	int store	//0001 OPcode mask
+	int add
+	int addi
+	int addc
+	int addci
+	int sub
+	int subi
+	int subc
+	int subci
+	int  and
+	int andi
+	int xor
+	int xori
+	int compl
+	int shl
+	int shla
+	int shr
+	int shra
+	int compr
+	int compri
+	int getstat
+	int putstat
+	int jump
+	int jumpl
+	int jumpe
+	int jumpg
+	int call
+	int return
+	int  read
+	int write
+	int halt		//11000 OP/code mask
+	int  noop 	//11001 OPcode mask
+
+
+}
+
+assembler::read{
+	
+}
+
+assembler::parse{
+
+}
+
+assembler::opcode{
+
+}
+
+assembler::checkError{
+
+}
+
+
+*/
