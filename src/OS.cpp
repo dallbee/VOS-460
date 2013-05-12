5cace3102fbb545e6ee6eda7094aa585ae1e0278
xCaldazar <xTaylorSanchez@gmail.com>
2013-05-12 12:33:27 -0700

    PCB and OS declared, and starting functions PCB could possibly be a struct?

--- src/OS.cpp
+++ src/OS.cpp
@@ -13,50 +13,116 @@
 #include "sys/assembler/Assembler.h"
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-	string fAndF = argv[1]; //filesAndFolders
-	fAndF = "../io/"+(fAndF.substr(0, fAndF.find_last_of("."))).append("/")+fAndF;
-	if(argc == 1){
-		printf ("Please specify a Program");
-	}
+PCB::PCB(pc(), ir(), sp(memSize - 1), base(), limit(progLimit), sr()){
+
+}
+
+PCB::PCB(string fileName, int tempBase, int tempLimit){
+	this -> fileName = fileName;
+	//input  = filename +".in";
+	//output = filename +"out";
+	stack  = filename + ".st";
+	reg[VirtualMachine::regSize];
+	base  = tempBase;
+	limit = tempLimit - tempBase;
+	ioTime = 0;
+	execTime = 0;
+}
+
+OS::OS(userTotal, osClock, tempLimit, tempBase, itdleTotal){
+
+}
+
+void OS::loadState(){ //loads PCB of running process, just before it starts
+	VM.pc = running -> pc + running -> base;
+	VM.sp = running -> sp;
+	VM.r = running -> reg;
+	VM.ir = running -> ir;
+	VM.readNum = running -> readNum;
+	VM.fileName = running -> fileName;
+	VM.base = running -> base;
+	VM.limit = running -> limit;
+}
 
 
-	// Assemble opcodes
-	try {
-		Assembler as("../src/sys/assembler/opcodes.lst");
-		as.build(fAndF);
-	} catch(const char* error) {
-		printf ("[Assembler Error] %s \n", error);
-		return 1;
-	}
+void OS::finish(){ //get total times
 
+}
 
-	//Load assembly code into memory
-	short mem[VirtualMachine::memSize]={0};
-	string programAs = fAndF;
-	programAs = (programAs.substr(0, programAs.find_last_of("."))).append(".o");
- 	ifstream asFile(programAs.c_str());
- 	if (! asFile.good()){
- 		printf ("OS: could not find assembly code file");
- 	}
- 	int asLine = 0, limit = 0;
- 	asFile >> asLine;
- 	while(!asFile.eof()){
- 		mem[limit++] = asLine;
- 		asFile >> asLine;
- 	}
+void OS::saveState(){ //saves PCB of running process upon finishing
 
+}
 
- 	//Execute the program
-	try {
-		VirtualMachine vm(fAndF, mem, limit);
-	} catch(const char* error) {
-		printf ("[VirtualMachine Error] %s \n", error);
-		return 1;
-	}
+void OS::scheduler(){
 
+}
 
-	printf("Program Complete!\n");
+void OS::prcoessFinish(){
+
+}
+
+void OS::run(){
+
+}
+
+void OS::load(){
+
+}
+
+int main(){
+	OS.os;
+	os.load();
+	os.scheduler();
+	os.run()
 	return 0;
 }
+
+//Old Code:
+
+// int main(int argc, char *argv[])
+// {
+// 	fAndF = argv[1]; //filesAndFolders
+// 	fAndF = "../io/"+(fAndF.substr(0, fAndF.find_last_of("."))).append("/")+fAndF;
+// 	if(argc == 1){
+// 		printf ("Please specify a Program");
+// 	}
+
+
+// 	// Assemble opcodes
+// 	try {
+// 		Assembler as("../src/sys/assembler/opcodes.lst");
+// 		as.build(fAndF);
+// 	} catch(const char* error) {
+// 		printf ("[Assembler Error] %s \n", error);
+// 		return 1;
+// 	}
+
+
+// 	//Load assembly code into memory
+// 	mem[VirtualMachine::memSize]={0};
+// 	programAs = fAndF;
+// 	programAs = (programAs.substr(0, programAs.find_last_of("."))).append(".o");
+//  	ifstream asFile(programAs.c_str());
+//  	if (! asFile.good()){
+//  		printf ("OS: could not find assembly code file");
+//  	}
+//  	int asLine = 0, limit = 0;
+//  	asFile >> asLine;
+//  	while(!asFile.eof()){
+//  		mem[limit++] = asLine;
+//  		asFile >> asLine;
+//  	}
+
+
+//  	//Execute the program
+// 	try {
+// 		VirtualMachine vm(fAndF, mem, limit);
+// 	} catch(const char* error) {
+// 		printf ("[VirtualMachine Error] %s \n", error);
+// 		return 1;
+// 	}
+
+
+// 	printf("Program Complete!\n");
+// 	return 0;
+// }
--- src/OS.h
+++ src/OS.h
@@ -13,20 +13,69 @@
 
 using namespace std;
 
+class PCB{
+public:
+	PCB();
+	friend class VirtualMachine;
+private:
+	PCB(string fileName, int tempBase, int tempLimit);
+	string fileName;
+	short reg[VirtualMachine::regSize];
+	short pc;
+	short ir;
+	short sp;
+	short base;
+	short limit;
+	short sr;
+//	string input;
+//	string output;
+	string stack;
+	string errorMessage;
+
+	//Process Specific
+	int execTime;
+	int waitTime;
+	int turnTime;
+	int ioTime;
+	int LargestStack;
+
+
+
+};
+
 class OS{
 public:
 	OS();
-	friend class VirtualMachine;
-private:
+	VirtualMachine VM;
+	Assembler as;
+	void run();
+	void load();
+	void loadState();
+	void saveState();
+	void scheduler();
+	void processFinish();
+	void finish();
 
-	//pcb is supposed to be private, then use ?
-	PCB(string fileName, int tempBase, int tempLimit);
-	queue<PCB *> readyQ, waitQ;
+
+private:
 	list<PCB *>  jobs;
+	queue<PCB *> readyQ, waitQ;
 	PCB* running;
 	int osClock;
 	short tempBase;
 	short tempLimit;
+	short exitCode;
+
+	//System Information
+	int userTotal;
+	int idleTotal;
+	int systemCpuUtil;
+	int userCpuUtil;
+	int throughput;
+
+	fstream osOutput;
+	fstream processStack;
+	//pcb is supposed to be private, then use ?
 
 
 	int asLine;
